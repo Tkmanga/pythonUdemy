b26ed37e367446ab4abb460f31677f8adff28a2d
joseTacacho <tacachojose@gmail.com>
2023-04-03 16:52:06 -0300

    logica de auth, termine con la conexion db

--- toder/todo/__init__.py
+++ toder/todo/__init__.py
@@ -8,17 +8,19 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY='mikey',
         DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
-        DATABASE_HOST_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
-        DATABASE_HOST_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
         DATABASE=os.environ.get('FLASK_DATABASE')
     )
 
     from . import db
 
     db.init_app(app)
+    from . import auth
+
+    app.register.blueprint(auth.bp)
 
     @app.route('/hola')
     def hola():
         return 'chanchito feliz'
-
     return app
