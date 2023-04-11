d15abb08d93ce62dd2c7f2711ccac9a4b937ad7d
joseTacacho <tacachojose@gmail.com>
2023-04-11 18:54:17 -0300

    terminamos con login, agregamos listado y add

--- toder/todo/auth.py
+++ toder/todo/auth.py
@@ -2,7 +2,7 @@ import functools
 
 from flask import (
 
-    Blueprint, flash, g, render_template, request, url_for, session
+    Blueprint, flash, g, render_template, request, url_for, session, redirect
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -20,14 +20,14 @@ def register():
         db, c = get_db()
         error = None
         c.execute(
-            'select id from username = %s'
+            'select id from user where username = %s', (username,)
         )
         if not username:
             error = 'Username es requerido.'
         if not password:
             error = 'Password es requerido.'
-        elif c.fetchone is not None:
-            error = 'Usuario {} se encuentra registrado'.format(username)
+        elif c.fetchone() is not None:
+            error = 'Usuario {} se encuentra registrado.'.format(username)
         if error is None:
             c.execute(
                 'insert into user(username,password) values (%s, %s)',
@@ -35,7 +35,7 @@ def register():
             )
             db.commit()
 
-            return redirect(url_for('auth_login'))
+            return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
 
@@ -48,7 +48,7 @@ def login():
         db, c = get_db()
         error = None
         c.execute(
-            'select * from user where username = %s', (username)
+            'select * from user where username = %s', (username,)
         )
         user = c.fetchone()
         if user is None:
@@ -58,6 +58,34 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('todo.index'))
         flash(error)
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'SELECT * FROM user WHERE id = %s', (user_id,)
+        )
+        g.user = c.fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth/login'))
+        return view(**kwargs)
+    return wrapped_view
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
---
+++ toder/todo/todo.py
@@ -0,0 +1,51 @@
+from flask import (Blueprint, flash, g, redirect,
+                   render_template, request, url_for)
+
+from werkzeug.exceptions import abort
+
+from todo.auth import login_required
+
+from todo.db import get_db
+bp = Blueprint('todo', __name__)
+
+
+@bp.route('/')
+@login_required
+def index():
+    db, c = get_db()
+    c.execute(
+        'SELECT t.id, t.description, u.username, t.completed, t.created_at FROM todo t JOIN user u on t.created_by = u.id order by t.created_at desc'
+    )
+    todos = c.fetchall()
+    return render_template('todo/index.html', todos=todos)
+
+
+@bp.route('/create', methods=['POST', 'GET'])
+@login_required
+def create():
+    if request.method == 'POST':
+        description = request.form['description']
+        error = None
+
+        if not description:
+            error = 'Description es requerida'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'insert into todo (description, completed, created_by)'
+                ' values (%s, %s, %s)',
+                (description, False, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
+
+    return render_template('todo/create.html')
+
+
+@bp.route('/update', methods=['POST', 'GET'])
+@login_required
+def update():
+    return ''
